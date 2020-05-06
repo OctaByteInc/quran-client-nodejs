4007ff8f17d87cb3e9e8ed30ee1b117992fed9cb
AxeemHaider <axeemhaider@gmail.com>
2020-05-06 10:12:20 +0500

    ready to publish

---
+++ index.js
@@ -0,0 +1,13 @@
+const Audio = require("./api/audio");
+const Ayah = require("./api/ayah");
+const Edition = require("./api/edition");
+const Image = require("./api/image");
+const Surah = require("./api/surah");
+const Translation = require("./api/translation");
+
+module.exports.Audio = Audio
+module.exports.Ayah = Ayah
+module.exports.Edition = Edition
+module.exports.Image = Image
+module.exports.Surah = Surah
+module.exports.Translation = Translation
